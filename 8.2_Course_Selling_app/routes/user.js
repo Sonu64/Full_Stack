36698249de2bd3f366be74d60e133119197e2cfb
Sonu64 <sonusantu64@gmail.com>
2025-04-08 09:23:17 +0530

    Created User Sign-In Route with JWT verification

--- 8.2_Course_Selling_app/routes/user.js
+++ 8.2_Course_Selling_app/routes/user.js
@@ -3,10 +3,11 @@
  */
 const express = require("express");
 const userRouter = express.Router();
-const { UserModel } = require("../db");
+const { UserModel, PurchasesModel } = require("../db");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const { z } = require("zod");
+const jwt = require("jsonwebtoken");
 
 // Middlewares
 userRouter.use(express.json());
@@ -48,9 +49,33 @@ userRouter.post("/signup", async (req, res) => {
 });
 
 // Sign-In User
-userRouter.post("/signin", (req, res) => {
+userRouter.post("/signin", async (req, res) => {
   // /api/v1/user/signin
-  res.json("Sign-In endpoint");
+  const givenEmail = req.body.email;
+  const givenPassword = req.body.password;
+
+  const foundUser = await UserModel.findOne({ email: givenEmail });
+  if (!foundUser) {
+    res.status(400).json({ error: "User not found !" });
+    return;
+  } else {
+    const passwordMatched = await bcrypt.compare(
+      givenPassword,
+      foundUser.password
+    );
+    if (!passwordMatched) {
+      res.status(400).json({ error: "Wrong Password !" });
+      return;
+    } else {
+      const token = jwt.sign(
+        {
+          userID: foundUser._id.toString(),
+        },
+        process.env.JWT_SECRET
+      );
+      res.status(200).json({ token: token });
+    }
+  }
 });
 
 // When user wants to see his purchased courses

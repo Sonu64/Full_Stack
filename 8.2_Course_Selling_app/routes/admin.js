67e693284c595b62f0aea4e01b7ad04e17f3bb1d
Sonu64 <sonusantu64@gmail.com>
2025-04-09 07:35:54 +0530

    Configured nodemon and added admin SignUp and SignIn Routes.

--- 8.2_Course_Selling_app/routes/admin.js
+++ 8.2_Course_Selling_app/routes/admin.js
@@ -1,19 +1,78 @@
 const express = require("express");
 const adminRouter = express.Router();
-const AdminModel = require("../db");
+const { AdminModel } = require("../db");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const { z } = require("zod");
+const jwt = require("jsonwebtoken");
 
 adminRouter.use(express.json());
-
 // Sign-Up Admin
-adminRouter.post("/signup", (req, res) => {
+adminRouter.post("/signup", async (req, res) => {
   // /api/v1/admin/signup
-  res.json("Admin Sign-Up endpoint");
+  try {
+    const requiredBody = z.object({
+      email: z.string().min(6).max(100).email(),
+      // The correct way is to first define it as a string and then apply .email()
+      fullName: z.string().min(6).max(200),
+      password: z.string().min(8).max(1000),
+      //Detailed Password Validation Logic skipped here
+    });
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+    if (parsedDataWithSuccess.success) {
+      const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+      await AdminModel.create({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        password: hashedPassword,
+      });
+      res.status(200).json({
+        message: `Successfully Signed Up ${req.body.fullName} as an Admin`,
+      });
+
+      return;
+    } else {
+      res.status(400).json({
+        message: "Invalid Credentials !",
+        error: parsedDataWithSuccess.error,
+      });
+      return;
+    }
+  } catch (err) {
+    res.status(409).json({ error: "Admin with that E-Mail already exists !" });
+    return;
+  }
 });
 
 // Sign-In Admin
-adminRouter.post("/signin", (req, res) => {
+adminRouter.post("/signin", async (req, res) => {
   // /api/v1/admin/signin
-  res.json("Admin Sign-In endpoint");
+  const givenEmail = req.body.email;
+  const givenPassword = req.body.password;
+
+  const foundAdmin = await AdminModel.findOne({ email: givenEmail });
+  if (!foundAdmin) {
+    res.status(400).json({ error: "Admin not found !" });
+    return;
+  } else {
+    const passwordMatched = await bcrypt.compare(
+      givenPassword,
+      foundAdmin.password
+    );
+    if (!passwordMatched) {
+      res.status(400).json({ error: "Wrong Admin Password !" });
+      return;
+    } else {
+      const token = jwt.sign(
+        {
+          AdminID: foundAdmin._id.toString(),
+        },
+        process.env.JWT_SECRET
+      );
+      res.status(200).json({ token: token });
+    }
+  }
 });
 
 // Add a course

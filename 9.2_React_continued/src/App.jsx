3ec715e5060ecc829b1d0c4ca857a5c2d36155e4
Sonu64 <sonusantu64@gmail.com>
2025-04-23 11:16:56 +0530

    When we don't use DEPENDENCY ARRAYS : Created a Stopwatch component and conditionally rendered it based on the value of count. useEffect() used, called once during component mounting. Manual cleanup by returning an anonymus function as whenever component unmounts this function gets called.

--- 9.2_React_continued/src/App.jsx
+++ 9.2_React_continued/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
       <b>Counter App</b>
       <h3>{count}</h3>
       <Counter count={count} setCount={setCount}></Counter>
+      {parseInt(count) % 2 === 0 ? <Stopwatch></Stopwatch> : null}
     </div>
   );
 }
@@ -29,6 +30,33 @@ const Counter = (props) => {
       <button onClick={increaseCount}>Increase count</button>
       <button onClick={decreaseCount}>Decrease count</button>
       <button onClick={resetCount}>Reset count</button>
+    </div>
+  );
+};
+
+const Stopwatch = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(function startWatch() {
+    alert("Mounted");
+    const clock = setInterval(() => {
+      setSeconds(function incrementSecond(seconds) {
+        return seconds + 1;
+      });
+    }, 1000);
+
+    // Manual cleanup by returning a function from useEffect, when component is unmounted
+    // automatically clock is cleared, else it exists even if clock is not present
+    // in Document.
+    return function () {
+      alert("Unmounted");
+      clearInterval(clock);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Stopwatch Running at {seconds} seconds.</h1>
     </div>
   );
 };

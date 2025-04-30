5074a356ce369d4655f8d45bcf613722f7ea992d
Sonu64 <sonusantu64@gmail.com>
2025-04-30 08:06:07 +0530

    Did Practice building Cards, Collapsibles and added ErrorBoundary Code using class based components

--- 9.2.2_children_errorBoundaries_and_practice/src/App.jsx
+++ 9.2.2_children_errorBoundaries_and_practice/src/App.jsx
@@ -1,5 +1,51 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+//import "./App.css";
+
+// ErrorBoundary component can be used as a Black box, as used by many orgs, as
+// Class Based components are rarely used now. ErrorBoundary component is the only
+// class based component with active usage in the industry.
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("Error caught:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div
+          style={{
+            margin: "auto",
+            marginTop: "40px",
+            border: "2px solid white",
+            width: "90%",
+            padding: "20px",
+            boxSizing: "border-box",
+          }}
+        >
+          <span
+            style={{
+              color: "red",
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+            }}
+          >
+            Something went wrong.
+          </span>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
 
 const Card = ({ children }) => {
   return (
@@ -16,6 +62,51 @@ const Card = ({ children }) => {
       {children}
     </div>
   );
+};
+
+const Collapsible = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div
+      style={{
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        padding: "",
+        margin: "40px",
+        boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)",
+        boxSizing: "border-box",
+      }}
+    >
+      <div
+        className="collapsibleTop"
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          gap: "1000px",
+          alignItems: "center",
+          borderBottom: "2px solid grey",
+        }}
+      >
+        <h3>{title}</h3>
+        <button
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+          style={{
+            padding: "7px 17px",
+            display: "block",
+          }}
+        >
+          {isOpen ? "Close" : "Open"}
+        </button>
+      </div>
+
+      {isOpen ? children : null}
+    </div>
+  );
+};
+
+const BuggyComponent = () => {
+  throw new Error("I crashed!");
 };
 
 const App = () => {
@@ -70,6 +161,17 @@ const App = () => {
           </Card>
         </div>
       </div>
+
+      <ErrorBoundary>
+        <BuggyComponent></BuggyComponent>
+      </ErrorBoundary>
+
+      <Collapsible title="Section 1">
+        <p style={{ padding: "15px" }}>This is the content of collapsible 1.</p>
+      </Collapsible>
+      <Collapsible title="Section 2">
+        <p style={{ padding: "15px" }}>This is the content of collapsible 2.</p>
+      </Collapsible>
     </>
   );
 };
